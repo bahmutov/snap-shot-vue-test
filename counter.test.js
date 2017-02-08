89b5dc30f5e9be7f75d8339a5070da1f083aa3f9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-08 01:01:12 -0500

    working test

--- counter.test.js
+++ counter.test.js
@@ -1,13 +1,25 @@
 import Vue from 'vue'
 import Counter from './counter'
+import snapshot from 'snap-shot'
 const renderer = require('vue-server-renderer').createRenderer()
 
-test('counter', () => {
+const render = vm => {
+  return new Promise((resolve, reject) => {
+    renderer.renderToString(vm, (err, str) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(str)
+    })
+  })
+}
+
+test('render counter', () => {
   const vm = new Vue({
     el: document.createElement('div'),
-    render: (h) => h(Counter),
+    render: h => h(Counter),
   })
-  renderer.renderToString(vm, (err, str) => {
-    expect(str).toMatchSnapshot();
-  })
+  return snapshot(
+    render(vm)
+  )
 })
